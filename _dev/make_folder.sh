1f7b057aa7138b2dc2dea77a2f4b8571deb89323
fabiantheblind <fabiantheblind@googlemail.com>
2012-09-24 19:51:31 +0200

    just a shell script

---
+++ _dev/make_folder.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+mkdir 001-002\ U1\ U2
+mkdir 003-013\ info
+mkdir 014-017\ books
+mkdir 018-103\ gitarren
+mkdir 104-151\ drums
+mkdir 152-187\ keys
+mkdir 188-197\ Zubehoer
+mkdir 198-239\ PA
+mkdir 240-255\ DJ
+mkdir 256-319\ studio
+mkdir 310-312\ piano\ galerie
+mkdir 322-337\ Brass
+mkdir 338_AGB
+mkdir 339-340\ U3\ U4
